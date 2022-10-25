a5e2f2192c5ce7aa39e8d37b00a36178875137ef
shashank <shashviru@gmail.com>
2022-10-25 14:25:53 +0530

    first commit

---
+++ diamond.py
@@ -0,0 +1,6 @@
+def diamond(h):
+    for x in range(h):
+        print(" " * (h - x), "*" * (2*x + 1))
+    for x in range(h - 2, -1, -1):
+        print(" " * (h - x), "*" * (2*x + 1))
+diamond(10)
